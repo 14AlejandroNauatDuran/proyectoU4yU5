fc8bd61e0116cd33c02fb2e6903595eb804d7556
IsraelPomolCouoh <israel.pomolcouoh@itsva.edu.mx>
2019-12-05 17:21:11 -0600

    actualizacion 3#

--- proyectoU4/src/AnalizadorLexicoU4.py
+++ proyectoU4/src/AnalizadorLexicoU4.py
@@ -62,3 +62,50 @@ t_ignore = r' \t'
 def t_NUMEROS(t):
     r'\d+.?\d*'
     return t
+
+def t_APE_PHP(t):
+    r'\<\?php'
+    return t
+
+def t_CIE_PHP(t):
+    r'\?\>'
+    return t
+
+def t_CADENA(t):
+    r'\".+\"'
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno +=len(t.value)
+
+def t_VARIABLES(t):
+    r'\$[a-zA-Z_]\w*'
+    return t
+
+def t_error(t):
+    print("Caracter ilegal '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def buscarFicheros(directorio):
+    ficheros = []
+    numArchivos = ''
+    respuesta = False
+    cont = 1
+
+    for base,dirs,files in os.walk(directorio):
+        ficheros.append(files)
+    
+    for file in files:
+        print(str(cont)+". "+ file)
+        cont = cont+1
+    
+    while respuesta == False:
+        numArchivos = input('\nNumero del test: ')
+        for file in files:
+            if file == files[int(numArchivos)-1]:
+                respuesta = True
+                break
+    
+    print("Has escogido \"%s\" \n" %files[int(numArchivos)-1])
+    return files[int(numArchivos)-1]
